57343adc16f179d4ba65266b064d3eabae1f1a50
Shahin992 <shahinkhan5979@gmail.com>
2023-10-18 02:43:59 +0600

    commit with footer section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,13 +4,13 @@ const Home = () => {
   return (
     <div>
       <div
-        className="hero min-h-screen mb-10 "
+        className="hero min-h-screen mb-10 bg-cover"
         style={{
           backgroundImage:
             "url(https://consumer.tcimg.net/assets/_next/static/images/hero-image-xl-da74f735c7d818b0df4545b958540724.jpg)",
         }}
       >
-        <div className="hero-overlay bg-opacity-20 bg-cover"></div>
+        <div className="hero-overlay bg-opacity-20 "></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-2xl">
             <h1 className="mb-5 text-5xl font-bold">Your Dream Car Awaits! Explore Now. </h1>
